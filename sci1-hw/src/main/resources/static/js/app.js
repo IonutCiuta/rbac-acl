985a8d8bd1db90cc4111eeaca8febdcdac146933
iciuta <ionut.ciuta@endava.com>
2017-11-01 14:18:12 +0200

    UI

--- sci1-hw/src/main/resources/static/js/app.js
+++ sci1-hw/src/main/resources/static/js/app.js
@@ -11,7 +11,28 @@ app.config(function ($routeProvider) {
 app.controller('AppController', ['$scope', function($scope) {
     console.log('App controller initialized');
 
-    $scope.methods = ['Create', 'Read', 'Write', 'Change permission'];
+    $scope.methods = [
+        {'text': 'Create', 'value': 0},
+        {'text': 'Read', 'value': 1},
+        {'text': 'Write', 'value': 2},
+        {'text': 'Change permission', 'value': 3}
+    ];
     $scope.rights = ['none', 'r', 'w', 'rw'];
     $scope.types = [{'value': 0, 'text': 'Folder'}, {'value': 1, 'text': 'File'}];
+
+    $scope.showContent = function(operation, type) {
+        console.log("showContent: " + operation + ", " + type);
+        return operation === 2 ||
+            (operation === 0 && type === 1);
+    };
+
+    $scope.showRights = function(operation) {
+        console.log("showRights: " + operation);
+        return operation === 3;
+    };
+
+    $scope.showType = function(operation) {
+        console.log("shotType: " + operation);
+        return operation === 3;
+    };
 }]);
